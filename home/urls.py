da1d7abd4ab9feef80ea8feb620ff33279300786
odinOnGit <tezaschhetri@gmail.com>
2025-05-24 15:52:36 +0530

    All files Updated

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HelloAPI
+from . import views
 
 urlpatterns = [
-  path('hello/', HelloAPI.as_view()),
+  path('blogposts/', views.BlogPostListCreate.as_view(), name='blogpost-view-create'),
+  path('blogposts/<int:pk>/', views.BlogPostRetrieveUpdateDestroy.as_view(), name='update')
 ]
--- home/views.py
+++ home/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import BlogPost
 from rest_framework.response import Response
+from .serializers import BlogPostSerializer
 from rest_framework.views import APIView
 
-class HelloAPI(APIView):
-  def get(self, request):
-    return Response({'message' : 'Hello, API!'})
+class BlogPostListCreate(generics.ListCreateAPIView):
+  queryset = BlogPost.objects.all()
+  serializer_class = BlogPostSerializer
+
+  def delete(self, request, *args, **kwargs):
+    BlogPost.objects.all().delete()
+    return Response({'status' : 204})
+
+class BlogPostRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+  queryset = BlogPost.objects.all()
+  serializer_class = BlogPostSerializer
+  lookup_field = 'pk'
